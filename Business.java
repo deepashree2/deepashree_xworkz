f3964d1d0259e7ae54b00c01eecb5885069b7667
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:31:21 +0530

    program2

---
+++ Business.java
@@ -0,0 +1,25 @@
+class Business{
+	Business()
+	{
+		System.out.println("We can earn money in Business");
+	}
+	
+}
+
+class BusinessRunner{
+	public static void main(String[] args)
+	{
+		System.out.println("Business runing");
+		new Business();
+		Business business = new Business();
+		Business business1 = new Business();
+		Business business2 = new Business();
+		Business business3 = new Business();
+		Business business4 = new Business();
+		Business business5 = new Business();
+		Business business6 = new Business();
+		Business business7 = new Business();
+		Business business8 = new Business();
+		Business business9 = new Business();
+	}
+}
