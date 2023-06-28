67e4bde87466788c03793873a6e6517ed6ecc2bc
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:46:05 +0530

    program8

---
+++ Mirror.java
@@ -0,0 +1,29 @@
+class Mirror{
+	
+	Mirror()
+	{
+		System.out.println("We can see our face in Mirror");
+	}
+}
+
+	
+
+
+class MirrorRunner{
+	public static void main(String[] args){
+	System.out.println("Mirror Runing....");
+	new Mirror();
+	Mirror mirror = new Mirror();
+	Mirror mirror1 = new Mirror();
+	Mirror mirror2 = new Mirror();
+	Mirror mirror3 = new Mirror();
+	Mirror mirror4 = new Mirror();
+	Mirror mirror5 = new Mirror();
+	Mirror mirror6 = new Mirror();
+	Mirror mirror7 = new Mirror();
+	Mirror mirror8 = new Mirror();
+	Mirror mirror9 = new Mirror();
+	
+	
+	}
+}	
