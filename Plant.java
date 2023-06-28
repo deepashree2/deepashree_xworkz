02df2afb6b39cd34052385224990c7156744869b
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:48:22 +0530

    program9

---
+++ Plant.java
@@ -0,0 +1,25 @@
+class Plant{
+	Plant()
+	{
+		System.out.println("Plant is there in road side and forest");
+	}
+}
+
+class PlantRunner{
+	public static void main(String[] args){
+		System.out.println("Plant runing");
+		new Plant();
+		Plant plant = 	new Plant();
+		Plant plant1 = 	new Plant();
+		Plant plant2 = 	new Plant();
+		Plant plant3 = 	new Plant();
+		Plant plant4 = 	new Plant();
+		Plant plant5 = 	new Plant();
+		Plant plant6 = 	new Plant();
+		Plant plant7 = 	new Plant();
+		Plant plant8 = 	new Plant();
+		Plant plant9 = 	new Plant();
+		
+	}
+}
+		
