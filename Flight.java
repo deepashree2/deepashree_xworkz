b80116a650a94271c2f40a49102fc05480c7fa9d
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:37:30 +0530

    program4

---
+++ Flight.java
@@ -0,0 +1,25 @@
+class Flight{
+	Flight()
+	{
+		System.out.println("Flight carry a things or man from one country to another");
+	}
+}
+	
+class FlightRunner{
+	public static void main(String[] args){
+	System.out.println("Flight is Running");
+	new Flight();
+	Flight flight = new Flight();
+	Flight flight1 = new Flight();
+	Flight flight2 = new Flight();
+	Flight flight3 = new Flight();
+	Flight flight4 = new Flight();
+	Flight flight5 = new Flight();
+	Flight flight6 = new Flight();
+	Flight flight7 = new Flight();
+	Flight flight8 = new Flight();
+	Flight flight9 = new Flight();
+
+
+	}
+}
