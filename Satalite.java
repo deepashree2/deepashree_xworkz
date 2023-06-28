89bac6e7d8f3c3b7abe2bfcd87324eb078a872da
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:50:54 +0530

    program10

---
+++ Satalite.java
@@ -0,0 +1,33 @@
+class Satalite{
+	
+	Satalite()
+	{
+		System.out.println("Receiving signals from Earth.");
+	}
+	
+}
+
+
+
+
+
+class SataliteRunner{
+	public static void main(String[] args){
+	
+	System.out.println("Satalite is running....");
+	new Satalite();
+	Satalite signal = new Satalite();
+	Satalite signal1 = new Satalite();
+	Satalite signal2 = new Satalite();
+	Satalite signal3 = new Satalite();
+	Satalite signal4 = new Satalite();
+	Satalite signal5 = new Satalite();
+	Satalite signal6 = new Satalite();
+	Satalite signal7 = new Satalite();
+	Satalite signal8 = new Satalite();
+	Satalite signal9 = new Satalite();
+	}
+}
+
+
+	
