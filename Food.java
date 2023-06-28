dfb202c1b3f68d512cf41595b786ace31229823b
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:40:00 +0530

    program5

---
+++ Food.java
@@ -0,0 +1,23 @@
+class Food{
+	Food()
+	{
+		System.out.println("various types of food in the World");
+	}
+}
+
+class FoodRunner{
+	public static void main(String[] args){
+		System.out.println("Food Running");
+		new Food();
+		Food food = new Food();
+		Food food1 = new Food();
+		Food food2 = new Food();
+		Food food3 = new Food();
+		Food food4 = new Food();
+		Food food5 = new Food();
+		Food food6 = new Food();
+		Food food7 = new Food();
+		Food food8 = new Food();
+		Food food9 = new Food();		
+	}
+}
