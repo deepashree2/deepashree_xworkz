95cf98ace71375dae3af7245c238ec5802cb949b
deepashrees2 <deepashreesl96@gmail.com>
2023-06-28 23:20:05 +0530

    bengaluru

---
+++ Bengaluru.java
@@ -0,0 +1,24 @@
+class Bengaluru{
+	Bengaluru()
+	{
+		System.out.println("Bengaluru is a capital of karnataka");
+	}
+}
+
+class BengaluruRunner{
+	public static void main(String[] args){
+		System.out.println("Bengaluru is running");
+		new Bengaluru();
+		Bengaluru bengaluru = new Bengaluru();
+		Bengaluru bengaluru1 = new Bengaluru();
+		Bengaluru bengaluru2 = new Bengaluru();
+		Bengaluru bengaluru3 = new Bengaluru();
+		Bengaluru bengaluru4 = new Bengaluru();
+		Bengaluru bengaluru5= new Bengaluru();
+		Bengaluru bengaluru6 = new Bengaluru();
+		Bengaluru bengaluru7 = new Bengaluru();
+		Bengaluru bengaluru8 = new Bengaluru();
+		Bengaluru bengaluru9 = new Bengaluru();
+		
+	}
+}
