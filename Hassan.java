fcbaf6b8daa413ed2a5d61ed4f56f7d411a8991f
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:43:59 +0530

    program7

---
+++ Hassan.java
@@ -0,0 +1,25 @@
+class Hassan{
+	Hassan()
+	{
+		System.out.println("Hassan is a historical place");
+	}
+}
+
+class HassanRunner{
+	public static void main(String[] args){
+	System.out.println("Hassan is running");
+	new Hassan();
+	Hassan hassan = new Hassan();
+	Hassan hassan1 = new Hassan();
+	Hassan hassan2 = new Hassan();
+	Hassan hassan3 = new Hassan();
+	Hassan hassan4 = new Hassan();
+	Hassan hassan5 = new Hassan();
+	Hassan hassan6 = new Hassan();
+	Hassan hassan7 = new Hassan();
+	Hassan hassan8 = new Hassan();
+	Hassan hassan9 = new Hassan();
+
+	
+	}
+}
