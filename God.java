846d81fe9782f878c00402909bb15c9b6b9ce286
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:42:05 +0530

    program6

---
+++ God.java
@@ -0,0 +1,29 @@
+class God{
+	God()
+	{
+		System.out.println("God is thier in everyone Heart");
+	}
+	
+	
+	
+}
+
+
+class GodRunner{
+	public static void main(String[] args){
+	System.out.println("God Runnig...");
+	new God();
+	God god = new God();
+	God god1 = new God();
+	God god2 = new God();
+	God god3 = new God();
+	God god4 = new God();
+	God god5 = new God();
+	God god6 = new God();
+	God god7 = new God();
+	God god8 = new God();
+	God god9 = new God();
+	
+	}
+
+}
