852e76035657e3c6c5bafca2782af755448be71e
deepashree2 <deepashreesl96@gmail.com>
2023-06-28 23:33:37 +0530

    program3

---
+++ Car.java
@@ -0,0 +1,23 @@
+class Car{
+	Car()
+	{
+		System.out.println("Using car we can go for trip");
+	}
+}
+
+class CarRunner{
+	public static void main(String[] args){
+		System.out.println("Car Running");
+		new Car();
+		Car car = new Car();
+		Car car1 = new Car();
+		Car car2 = new Car();
+		Car car3 = new Car();
+		Car car4 = new Car();
+		Car car5 = new Car();
+		Car car6 = new Car();
+		Car car7 = new Car();
+		Car car8 = new Car();
+		Car car9 = new Car();		
+	}
+}
