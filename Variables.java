4b682e409ed221cd18d8240a745a02180abb9d6b
deepashree2 <deepashreesl02@gmail.com>
2023-07-11 22:42:42 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,19 @@
+package programs;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int a =23;
+		int b =45;
+		int sum = a+b;
+		float sub = a-b;
+		int mul = a*b;
+		float div = a/b;
+		System.out.println("Sum of a and b is :"+sum);
+		System.out.println("Subtract of a and b is :"+sub);
+		System.out.println("multiplication of a and b is :"+mul);
+		System.out.println("division of a and b is :"+div);
+
+	}
+
+}
